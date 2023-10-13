f05fbdb4a1fcad5ccc886a9f869c75c29fda429f
itsmuhammadasif <2019cs219@student@uet.edu.pk>
2023-10-13 18:39:47 +0500

    Updated the model structure

--- server/models/blog.js
+++ server/models/blog.js
@@ -32,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     author: {
-      type:  DataTypes.STRING,
+      type: DataTypes.STRING,
       allowNull: false
     },
     createdAt: {
