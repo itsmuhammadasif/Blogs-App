f949c4d744aca2b38df4be27c73b2f0c7163aa0c
itsmuhammadasif <2019cs219@student@uet.edu.pk>
2023-10-13 18:41:20 +0500

    Updated to test swagger documenattion

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { User } = require('../models')
-const { body, validationResult } = require('express-validator');
 authControllers = require('../controllers/auth.controllers')
-const { validateSignup, validateLogin} = require('../middleware/auth.validation.middleware')
+const { validateSignup, validateLogin } = require('../middleware/auth.validation.middleware')
 
 
 router.post('/signup', validateSignup, authControllers.createUser);
