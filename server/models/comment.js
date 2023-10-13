edb5b0fa894ee7989bb487cbb16cc26893c06312
itsmuhammadasif <2019cs219@student@uet.edu.pk>
2023-10-13 18:39:59 +0500

    Updated the model structure

--- server/models/comment.js
+++ server/models/comment.js
@@ -4,7 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Comment extends Model {
-   
+
     static associate(models) {
       Comment.belongsTo(models.User, {
         foreignKey: 'userId',
