6b7de73e679cd11107f1eca5bbb5fe716d1b0023
itsmuhammadasif <2019cs219@student@uet.edu.pk>
2023-10-13 18:41:40 +0500

    Updated to final code

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { Blog } = require('../models');
 const authMiddleware = require('../middleware/authMiddleware');
-const client = require('../config/dbconnect')
 blogControllers = require('../controllers/blog.controllers')
-const {validateCreateBlog} = require('../middleware/blogs.validation.middleware')
+const { validateCreateBlog } = require('../middleware/blogs.validation.middleware')
 
 
 router.get('/blogs', authMiddleware, blogControllers.getBlogs);
 
-router.get('/blogs/:id', blogControllers.getBlogsById);
+router.get('/blogs/:id',  blogControllers.getBlogsById);
 
-router.delete('/blogs/:id', blogControllers.deleteBlogsById)
+router.delete('/blogs/:id',  blogControllers.deleteBlogsById)
 
 router.post('/create', authMiddleware, validateCreateBlog, blogControllers.createBlog);
 
